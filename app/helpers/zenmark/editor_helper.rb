9755428d1e9d8060aab608efc4506d73d5932367
cloudqubes <cloud.qubes@gmail.com>
2025-07-18 18:49:12 +0530

    replace text

--- app/helpers/zenmark/editor_helper.rb
+++ app/helpers/zenmark/editor_helper.rb
@@ -4,7 +4,9 @@ module Zenmark
       content_tag :div, class: "zenmark-editor", data: { controller: 'zenmark-editor' } do
         render("zenmark/toolbar") +
         render("zenmark/link_dialog") +
-        file_field_tag(:file, style: "display: none;", data: { "zenmark-editor-target" => "fileInput", action: "change->zenmark-editor#uploadImage" }) +
+        # file_field_tag(:file, style: "display: none;", data: { "zenmark-editor-target" => "fileInput", action: "change->zenmark-editor#uploadImage" }) +
+        form.file_field(:images, style: "display: none;", data: { "zenmark-editor-target" => "fileInput", action: "change->zenmark-editor#submitForm" }) +
+
         form.text_area(field, data: {"zenmark-editor-target": "mdtextarea" }, class: "zenmark-textarea") +
         content_tag(:div, "", id: "zenmark-preview", data: { "zenmark-editor-target" => "previewArea" })
       end
--- app/javascript/controllers/zenmark_editor_controller.js
+++ app/javascript/controllers/zenmark_editor_controller.js
@@ -58,7 +58,7 @@ export default class extends Controller {
     event.preventDefault()
     const text = this.linkTextTarget.value
     const url = this.linkUrlTarget.value
-    this.wrapText(`[${text}](${url})`, "")
+    this.replaceText(`[${text}](${url})`)
     this.closeLinkDialog(event)
   }
 
@@ -81,6 +81,12 @@ export default class extends Controller {
       this.insertText(`![${file.name}](/rails/active_storage/blobs/${data.signed_id}/${file.name})`)
     })
   }
+
+  submitForm(event) {
+    event.preventDefault()
+    const form = event.target.closest("form")
+    form.submit()
+  } 
 
   preview(event) {
     event.preventDefault()
@@ -150,16 +156,15 @@ export default class extends Controller {
     textarea.selectionStart = textarea.selectionEnd = start + newText.length
   }
 
-  replaceText(star) {
+  replaceText(newText) {
     const textarea = this.mdtextareaTarget
     const start = textarea.selectionStart
     const end = textarea.selectionEnd
-    const selectedText = textarea.value.substring(start, end)
-    const newText = `${prefix}${selectedText}${suffix}`
+    // const selectedText = textarea.value.substring(start, end)
+    // const newText = `${prefix}${selectedText}${suffix}`
 
     textarea.setRangeText(newText, start, end)
     textarea.focus()
     textarea.selectionStart = textarea.selectionEnd = start + newText.length
-
   }
 }
